c7dc941a98f289839cac016098a213724f019756
julianolandim <juliano@ibg.ind.br>
2020-03-22 15:08:52 -0300

    Initial commit

---
+++ src/test/java/br/ind/ibg/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ind.ibg.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
